8af6a6c0c152eb14aa314d94c2a2d7d05a0306f6
Steve Lisens <steve.lisens@vdabcampus.be>
2024-10-29 15:43:41 +0100

    Add the task Waarnemer

---
+++ src/be/vdab/jpfhfdst8/Waarnemer.java
@@ -0,0 +1,35 @@
+package be.vdab.jpfhfdst8;
+
+public class Waarnemer {
+    private int maxTemp = Integer.MIN_VALUE;
+    private int minTemp = Integer.MAX_VALUE;
+    private int aantalWaarnemingen;
+    private double somTemp;
+
+    public int getMaxTemp() {
+        return aantalWaarnemingen > 0 ? maxTemp : 0;
+    }
+
+    public int getMinTemp() {
+        return aantalWaarnemingen > 0 ? minTemp : 0;
+    }
+
+    public int getAantalWaarnemingen() {
+        return aantalWaarnemingen;
+    }
+
+    public double getGemTemp() {
+        return aantalWaarnemingen > 0 ? somTemp / aantalWaarnemingen : 0;
+    }
+
+    public void registreer(int temp) {
+        somTemp += temp;
+        aantalWaarnemingen++;
+        if (temp > maxTemp) {
+            maxTemp = temp;
+        }
+        if (temp < minTemp) {
+            minTemp = temp;
+        }
+    }
+}
